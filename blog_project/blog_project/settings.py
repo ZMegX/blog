f73d55962e96d952ab12d3b2353122eb7e9de7c2
ZMegX <picardomegan@gmail.com>
2025-08-12 01:19:15 +0100

    urls added to users and crispy form

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    "crispy_forms", 
+    "crispy_bootstrap5", 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# users app
+LOGIN_URL = 'login'
