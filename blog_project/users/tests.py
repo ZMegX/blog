63beb0f94e210533b41fa139c26e92222fdcac9e
ZMegX <picardomegan@gmail.com>
2025-08-12 15:47:05 +0100

    bug fix - profile pic - implementing users.signals

--- blog_project/users/tests.py
+++ blog_project/users/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth.models import User
+from users.models import Profile
+
+for user in User.objects.all():
+    Profile.objects.get_or_create(user=user)
