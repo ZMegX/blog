0f2434c48fb6a0ea24d144612ebaf77d0816dfef
ZMegX <picardomegan@gmail.com>
2025-08-14 21:12:45 +0100

    flatten Django structure for deployment

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
-ALLOWED_HOSTS = ['https://blog-3eyt.onrender.com']
+ALLOWED_HOSTS = ['https://blog-3eyt.onrender.com', '127.0.0.1']
 
 
 # Application definition
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
