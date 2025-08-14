f29877ad9af635921820be04e5b8acb7a4d86e14
ZMegX <picardomegan@gmail.com>
2025-08-14 23:28:43 +0100

    bug fix - url when editing profile OK

--- users/views.py
+++ users/views.py
@@ -33,7 +33,7 @@ def profile(request):
       u_form.save()
       p_form.save()
       messages.success(request, f'Your account has been updated') #Changes here
-      return redirect('dashboard') #Changes here
+      return redirect('profile') #Changes here
   else:
     u_form = UserUpdateForm(instance=request.user)
     p_form = ProfileUpdateForm(instance=request.user.profile)
