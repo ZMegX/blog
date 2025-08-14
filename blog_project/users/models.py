d17f89e0ca15973ceebc7b5e1ef62ae28eb9d388
ZMegX <picardomegan@gmail.com>
2025-08-14 19:20:08 +0100

    model and form updated

--- blog_project/users/models.py
+++ blog_project/users/models.py
@@ -4,11 +4,14 @@ from PIL import Image # Change here from pillow import Image
 
 # Create your models here.
 class Profile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+  first_name = models.CharField(max_length=200, null=True)
+  last_name = models.CharField(max_length=200, null=True)
+  email = models.EmailField(max_length = 254, default='example@email.com')
   image = models.ImageField(default='default.png', upload_to='profile_pics')
 
   def __str__(self):
-    	return f'{self.user.username} Profile'
+    	return f'{self.user.username}'
   
   """
     Changes here explained below
