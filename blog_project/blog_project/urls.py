f59c2f78315b3670414642e2edbe809fb527f595
ZMegX <picardomegan@gmail.com>
2025-08-14 19:18:39 +0100

    new features added - change/reset password - register - user views added

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -1,33 +1,25 @@
-"""
-URL configuration for blog_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from users import views as user_views
+from . import views
+from users import views as user_views
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls")),
-    path("", include("users.urls")),
+    path('blog/', include('blog.urls')),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', user_views.register, name='register'),
+    path('accounts/', include("django.contrib.auth.urls")),    # for the authorized views
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', html_email_template_name='users/emails/password_reset_email.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
