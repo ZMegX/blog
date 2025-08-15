96aa917eaefa7cc92c484e1de55399700c26868d
ZMegX <picardomegan@gmail.com>
2025-08-15 22:41:12 +0100

    blog layout enhanced

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,10 @@
 # blog/forms.py
 
 from django import forms
-from .models import Comment
+from .models import Comment, Post
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['body']
-        
+
