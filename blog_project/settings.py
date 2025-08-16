c41b4da9304b6bcb5e0dae1f10be5584ea407e62
ZMegX <picardomegan@gmail.com>
2025-08-16 13:44:04 +0100

    condition for coudinary if local run dot env

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
 
-load_dotenv() 
+if os.environ.get("DJANGO_DEVELOPMENT") == "True":
+    from dotenv import load_dotenv
+    load_dotenv()
+
 CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
