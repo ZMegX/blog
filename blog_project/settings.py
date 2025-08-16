540c4ad7da07d14bb3cd5e2ce78cba5061afb55f
ZMegX <picardomegan@gmail.com>
2025-08-16 14:04:23 +0100

    settings updated for cloudinary

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # static files on render
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
