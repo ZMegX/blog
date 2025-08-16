c00ea5773191f2a128a6e6082217ba43e349e3da
ZMegX <picardomegan@gmail.com>
2025-08-16 18:59:30 +0100

    send email from GMAIL

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -166,3 +166,11 @@ MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'picardomegan@gmail.com'          # your Gmail address
+EMAIL_HOST_PASSWORD = 'xxhrvsixiystazsr'    # your Gmail app password
+DEFAULT_FROM_EMAIL = 'Blog_project'
